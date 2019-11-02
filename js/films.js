ab971ba142050d1487c91b84c8012a5554bdcfac
svetkinsky <tebsv@inbox.ru>
2019-11-02 10:11:31 +0300

    теущая версия, СВОИМ ТЕМПОМ разбираюсь в выводе через цикл. Циклы я згаю, а вот вывод данных через js в html сейчас укладываю в голове

---
+++ js/films.js
@@ -0,0 +1,55 @@
+
+const ganar_1 ='фантастика' 
+const ganar_2 = 'боевик'
+const ganar_3 = 'приключения'
+const ganar_4 = 'фэнтези'  
+const ganar_5 = 'драма'
+const ganar_6 = 'комедия'
+const ganar_7 = 'мультфильм'
+
+let start_film_1 = '10:00'
+let start_film_2 = '12:00'
+let start_film_3 = '14:00'
+let start_film_4 = '16:00'
+
+let name_film_1 = 'Человек-паук'
+let name_film_2 = 'Собачья жизнь 2'
+let name_film_3 = 'История игрушек 4'
+let name_film_4 = 'Люди в черном: Интернэшнл'
+
+let ganar_film_1 = ganar_1 + ' ' + ganar_2 + ' ' + ganar_3
+let ganar_film_2 = ganar_4 + ' ' + ganar_5 + ' ' + ganar_6
+let ganar_film_3 = ganar_7 + ' ' + ganar_4 + ' ' + ganar_6
+let ganar_film_4 = ganar_1 + ' ' + ganar_2 + ' ' + ganar_6
+
+let element_start_film1 = document.getElementById('start__film1')
+let element_name_film1 = document.getElementById('name__film1')
+let element_ganar_film1 = document.getElementById('ganar__film1')
+
+element_start_film1.innerHTML = start_film_1
+element_name_film1.innerHTML = name_film_1
+element_ganar_film1.innerHTML = ganar_film_1
+
+let element_start_film2 = document.getElementById('start__film2')
+let element_name_film2 = document.getElementById('name__film2')
+let element_ganar_film2 = document.getElementById('ganar__film2')
+
+element_start_film2.innerHTML = start_film_2
+element_name_film2.innerHTML = name_film_2
+element_ganar_film2.innerHTML = ganar_film_2
+
+let element_start_film3 = document.getElementById('start__film3')
+let element_name_film3 = document.getElementById('name__film3')
+let element_ganar_film3 = document.getElementById('ganar__film3')
+
+element_start_film3.innerHTML = start_film_3
+element_name_film3.innerHTML = name_film_3
+element_ganar_film3.innerHTML = ganar_film_3
+
+let element_start_film4 = document.getElementById('start__film4')
+let element_name_film4 = document.getElementById('name__film4')
+let element_ganar_film4 = document.getElementById('ganar__film4')
+
+element_start_film4.innerHTML = start_film_4
+element_name_film4.innerHTML = name_film_4
+element_ganar_film4.innerHTML = ganar_film_4
